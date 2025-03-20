291f6930cfeaad7bf841456f99bf80d228ff84b8
YoungHun-Kwon <tv3658@naver.com>
2025-03-21 03:30:23 +0900

    week02

---
+++ 2학년/2학기/웹 서버/week02/reply.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['username'];
+ echo 'username:'. $name. "<br/>";
+
+ $name = $_POST['cars'];
+ echo 'cars:'. $name. "<br/>";
+
+ $name = $_POST['vehicle1'];
+ echo 'vehicle1:'. $name. "<br/>";
+
+ $name = $_POST['vehicle2'];
+ echo 'vehicle2:'. $name. "<br/>";
+
+ $name = $_POST['vehicle3'];
+ echo 'vehicle3:'. $name. "<br/>";
+
+ $name = $_POST['fav_language'];
+ echo 'fav_language:'. $name. "<br/>";
+ ?>
+ <P> 20213032 권영훈 </P>
+
